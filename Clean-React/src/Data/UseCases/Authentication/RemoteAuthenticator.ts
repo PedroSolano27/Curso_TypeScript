fb7ed402d3198fb09db1e08c57e83c3828b66591
PedroSolano27 <pedro_ssolano@yahoo.com>
2025-05-15 10:48:50 -0300

    Adicionado resposta de caso de sucesso 200

--- Clean-React/src/Data/UseCases/Authentication/RemoteAuthenticator.ts
+++ Clean-React/src/Data/UseCases/Authentication/RemoteAuthenticator.ts
@@ -1,27 +1,27 @@
 // Realiza a autenticação de usuário de forma remota
 import { HttpPostClient } from "Data/Protocols/Http/HttpPostClient";
-import { Authentication } from "Domain/UseCases/Authenticator";
+import { Authentication, Authenticator } from "Domain/UseCases/Authenticator";
 import { HttpStatusCode } from "Data/Protocols/Http/HttpResponseClient";
 import { InvalidCredencialsError } from "Domain/Errors/InvalidCredencialsError";
 import { UnexpectedError } from "Domain/Errors/UnexpectedError";
 import { AccountModel } from "Domain/Models/AccountModel";
 
-export class RemoteAuthenticator {
+export class RemoteAuthenticator implements Authenticator{
     constructor (
         private readonly url: string,
         private readonly PostClient: HttpPostClient <Authentication, AccountModel>,
     ) {}
 
-    async auth(params: Authentication): Promise <void> {
+    async auth(params: Authentication): Promise <AccountModel> {
         // Pega a resposta do POST
         const response = await this.PostClient.post({
             url: this.url,
-            body: params
+            body: params,
         });
 
         // Verifica o código e devolve a resposta apropriada
         switch(response.status) {
-            case (HttpStatusCode.ok): break;
+            case (HttpStatusCode.ok): return response.body;
             case (HttpStatusCode.unauthorized): throw new InvalidCredencialsError;
             default: throw new UnexpectedError; 
         }
