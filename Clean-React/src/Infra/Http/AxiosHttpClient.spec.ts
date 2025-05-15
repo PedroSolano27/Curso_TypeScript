1126cc01ef0820019aa57f599c6a74dfc137933b
PedroSolano27 <pedro_ssolano@yahoo.com>
2025-05-15 13:31:51 -0300

    Adicionado biblioteca Jest para testes

---
+++ Clean-React/src/Infra/Http/AxiosHttpClient.spec.ts
@@ -0,0 +1,16 @@
+// Teste do Client Http com Axios
+import { AxiosHttpClient } from "./AxiosHttpClient";
+import axios from "axios";
+import faker from "faker";
+
+jest.mock("Axios");
+const AxiosMock = axios as jest.Mocked <typeof axios>;
+
+describe('AxiosHttpClient', () => { 
+    test('Should call axios with correct URL', async() => {
+        const url = faker.internet.url()
+        const sut = new AxiosHttpClient();
+        await sut.post({ url });
+        expect (AxiosMock).toHaveBeenCalledWith(url);
+    })
+})
---
+++ Clean-React/src/Infra/Http/AxiosHttpClient.ts
@@ -0,0 +1,9 @@
+// Client Http com Axios
+import { PostParams } from "Data/Protocols/Http/HttpPostClient";
+import axios from "axios";
+
+export class AxiosHttpClient {
+    async post (params: PostParams <any>): Promise <void> {
+        await axios(params.url)
+    }
+}
