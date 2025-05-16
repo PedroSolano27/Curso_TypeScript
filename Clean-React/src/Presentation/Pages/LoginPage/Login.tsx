7af6b88c0b5d33654dee67feaa6ad48363ddd9a0
PedroSolano27 <pedro_ssolano@yahoo.com>
2025-05-16 14:31:09 -0300

    Adicionado protótipo visual de página de login

--- Clean-React/src/Presentation/Pages/LoginPage/Login.tsx
+++ Clean-React/src/Presentation/Pages/LoginPage/Login.tsx
@@ -1,12 +1,28 @@
 // Página de Login
 import React from "react";
 import "./Login.css";
+import Spinner from "../../Components/Spinner/Spinner";
 
 function Login() {
   return (
-    <section id="login">
-      <h2>Página de Login</h2>
-    </section>
+    <form id="login">
+      <h2>Login</h2>
+
+      <section className="form-inputs">
+        <input className="" type="email" name="email" placeholder="Digite seu Email" required/>
+        <input className="" type="password" name="password" placeholder="Digite sua senha" required/>
+      </section>
+
+      <section className="login-inputs">
+        <button type="submit">Entrar</button>
+        <a href="/">Criar Conta</a>
+      </section>
+
+      <section className="status-msg">
+        <Spinner className="spinner"/>
+        <p>Exemplo de erro</p>
+      </section>
+    </form>
   );
 }
 
